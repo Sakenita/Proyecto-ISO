2369084941d772a12001c9213e65929b3f85bde8
SomeOne1010Ale <cbbe.claudiaalejandra.caceres.co@unifranz.edu.bo>
2021-10-17 16:59:34 -0400

    Updated

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if(isset($_POST['login']))
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Estacionamiento Universidad Unifranz :)</title>
+	<title>Estacionamiento universidad Unifranz :)</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/datepicker3.css" rel="stylesheet">
 	<link href="css/styles.css" rel="stylesheet">
